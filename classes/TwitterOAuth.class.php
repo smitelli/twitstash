4255444434f4f45226dda6e6c5bb45f144594143
Scott Smitelli <scott@smitelli.com>
2017-12-09 14:20:52 -0500

    Submodule moved around

--- classes/TwitterOAuth.class.php
+++ classes/TwitterOAuth.class.php
@@ -1,6 +1,6 @@
 <?php
 
   // Stub class to load the proper file in the submodule
-  require APP_DIR . '/twitteroauth/twitteroauth/twitteroauth.php';
+  require APP_DIR . '/twitteroauth/src/TwitterOAuth.php';
 
 ?>
